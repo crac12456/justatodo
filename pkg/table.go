b2fec835bdf6e9319aedfabea0db0f86d24a126b
Eliel gateway <eligonzamcraf@gmail.com>
2025-11-11 22:18:30 -0500

    apenas hice algo xd

--- pkg/table.go
+++ pkg/table.go
@@ -1,38 +1,51 @@
 package pkg
 
 import (
+	//"container/heap"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
-	tw "github.com/olekukonko/tablewriter"
+	tw "github.com/olekukonko/tablewriter" // Tablewriter, permite crear tablas de forma sencilla en ASCII
 )
 
 // NOTE: Esta funcion se encarga de mostrar todas las tareas
 // TODO: Transformar los datos de la db a datos en un string
 // TODO: Hacer una forma de que se reste la fecha actual de la fecha de vencimiento
+
 func ShowItems(db *sql.DB) {
+	// Esta funcion se encarga de mostrar las distintas tareas, hacer la tabla y todo esok
 
 	// conseguimos la cantidad de bases de datos
 	count, err := CountDB(db)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if count < 1 {
+	if count < 1 { // Funcion para comprobar que existan almenos mas de una tarea
+		//TODO: Podria hacer que envie un valor predeterminado en caso de no tener tareas
+
 		fmt.Print("No tiene tareas")
 	}
 
-	rows, err := db.Query("SELECT id, name, description, priority, state, created, due")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+	//NOTE: Mi idea aqui es crear el tipico bucle for para enviar los datos a un slice y de ese slice a un slice 2d
+	//ese slice 2d sera lo que imprimira la tabla 
 
-	header := []string{"ID", "Nombre", "Descripcion", "Prioridad", "Vencimiento", "Estado"}
+	var allTask [][]string 
+
+	for id, id >= count {
+		var buffer []string
+		row, err := db.Query(
+			"SELECT id, name, description, priority, state, created, due", id
+			).Scan(&buffer)
+	}
+
+	tableHead := []string{"ID", "Nombre", "Descripcion", "Prioridad", "Vencimiento", "Estado"} //Los datos en encabezado
 
 	table := tw.NewWriter(os.Stdout)
+	table.Header(tableHead)
 
 }
